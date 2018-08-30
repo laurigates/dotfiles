199027b3efc0e1013e14ac705604d1c9c60a33cb
Lauri Gates <lauri.gates@frozenbyte.com>
2018-08-30 14:27:35 +0300

    Alias for editing vimrc

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,3 +3,6 @@ type nvim >/dev/null 2>&1 && alias vim="nvim"
 
 # make common r10k deploy easier
 alias deploy='sudo r10k deploy environment -v info'
+
+# easy vimrc editing
+alias vimrc='vim ~/.vimrc'
