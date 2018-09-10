32eff4cd286cf286d1d0bc084234d788a8addb58
Lauri Gates <lauri.gates@gmail.com>
2018-09-10 15:21:24 +0300

    Started working on package installation script

---
+++ dependencies.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -e
+packages=(
+fonts-font-awesome fonts-hack fonts-powerline
+zsh
+tmux tmuxinator
+i3 rofi compton
+git
+playerctl google-play-music-desktop-player
+)
+
+for package in ${packages[@]}; do
+    if ! dpkg -s $package > /dev/null 2>&1; then
+        echo "$package is missing. Install it (y/n)?"
+        read answer
+        if [ "$answer" != "${answer#[Yy]}" ]; then
+            sudo apt-get install $package
+        fi
+    else
+        echo "$package found."
+    fi
+done
