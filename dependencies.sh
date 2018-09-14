e87c81b4887dc418a3e74370e2ce23fbd5fa0498
Lauri Gates <lauri.gates@gmail.com>
2018-09-14 16:57:57 +0300

    i3 and rofi related changes

--- dependencies.sh
+++ dependencies.sh
@@ -3,7 +3,7 @@ packages=(
 fonts-font-awesome fonts-hack fonts-powerline
 zsh
 tmux tmuxinator
-i3 i3-wm i3blocks i3lock i3lock-fancy i3status rofi compton
+i3 i3-wm i3blocks i3lock i3lock-fancy i3status rofi compton wmctrl xrdb
 git
 playerctl google-play-music-desktop-player
 )
