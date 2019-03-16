94d85db17a8a20a0a146f973c9dced5283ee8db7
Lauri Gates <lauri.gates@gmail.com>
2019-03-16 15:02:22 +0200

    Update dependencies

--- dependencies.sh
+++ dependencies.sh
@@ -3,6 +3,7 @@ aptpackages=(
 fonts-font-awesome fonts-hack fonts-powerline
 zsh
 tmux
+neovim
 i3 i3-wm i3blocks i3lock i3lock-fancy i3status rofi compton wmctrl x11-xserver-utils dunst feh
 git
 playerctl google-play-music-desktop-player
