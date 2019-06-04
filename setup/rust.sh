770a7f59a7f5ac0257a4e2e278ab2ada5a197f67
Lauri Gates <lauri.gates@gmail.com>
2019-06-04 15:28:41 +0300

    Add checks to rust/cargo setup

--- setup/rust.sh
+++ setup/rust.sh
@@ -2,7 +2,12 @@
 
 if command -v cargo; then
     mkdir -p ~/.cargo/bin
-    strip ~/.cargo/bin/*
+    # only run strip if binaries exist
+    if [ $(find ~/.cargo/bin -type f | wc -l) -gt 0 ]; then
+        strip ~/.cargo/bin/*
+    else
+        echo "No cargo binaries found. Skipping stripping them."
+    fi
 else
     echo "cargo not installed"
 fi
