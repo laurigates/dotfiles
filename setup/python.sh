19c09c9c2f717b46dbec8e61c4d4d38d9554b84c
Lauri Gates <lauri.gates@gmail.com>
2019-07-15 10:46:12 +0300

    Make python setup quiet

--- setup/python.sh
+++ setup/python.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 if command -v pip > /dev/null; then
-    pip install --user --upgrade -r requirements.txt
+    pip install --user --upgrade -q -r requirements.txt
 else
     echo "pip not installed"
 fi
 
 if command -v pip3 > /dev/null; then
-    pip3 install --user --upgrade -r requirements.txt
+    pip3 install --user --upgrade -q -r requirements.txt
 else
     echo "pip3 not installed"
 fi
