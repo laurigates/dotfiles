d5087035ca014f9525eb8a3f92831a8e8d310261
Lauri Gates <lauri.gates@frozenbyte.com>
2019-10-15 14:26:07 +0300

    Fix coc extension install

--- setup/nvim.sh
+++ setup/nvim.sh
@@ -2,7 +2,7 @@
 
 if command -v nvim > /dev/null; then
     nvim +PlugInstall +PlugUpdate +UpdateRemotePlugins +qa --headless
-    nvim +CocInstall coc-eslint coc-yaml coc-css coc-python coc-tsserver coc-json coc-solargraph coc-snippets
+    nvim +CocInstall coc-eslint coc-yaml coc-css coc-python coc-tsserver coc-json coc-solargraph coc-snippets +qa --headless
 else
     echo "nvim not installed"
 fi
