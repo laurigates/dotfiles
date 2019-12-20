397c7e4033aa69f4b80a7cbec40eae4aaa8f26d8
Lauri Gates <lauri.gates@gmail.com>
2019-12-20 13:49:06 +0200

    Fix and improve ruby setup script

--- setup/ruby.sh
+++ setup/ruby.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-if command -v bundle > /dev/null; then
+if ! command -v rbenv > /dev/null; then
     cd ~/.rbenv && src/configure && make -C src
     eval "$(~/.rbenv/bin/rbenv init -)"
-    cd ~/dotfiles && gem install bundler && bundle install
-else
-    echo "bundle not installed"
+    rbenv install 2.6.5
+    rbenv global 2.6.5
 fi
+
+if ! command -v bundle > /dev/null; then
+    cd ~/dotfiles && gem install bundler
+fi
+
+bundle install
