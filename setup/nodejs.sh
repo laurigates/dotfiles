d8f353baf498c7505e0279ffe420ae7d1b269268
Lauri Gates <lauri.gates@gmail.com>
2020-04-21 19:07:54 +0300

    Update nvm install version

--- setup/nodejs.sh
+++ setup/nodejs.sh
@@ -11,7 +11,7 @@ else
 
     # prompt whether or not to install nvm
     if setup/prompt.sh; then
-        curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+        curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
     fi
 fi
 
