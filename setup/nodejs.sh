250e7bbfeedef7cba6464ad314c030318587c9e0
Lauri Gates <lauri.gates@gmail.com>
2020-06-15 16:01:15 +0300

    Simplify nodejs setup

--- setup/nodejs.sh
+++ setup/nodejs.sh
@@ -1,19 +1,9 @@
 #!/bin/bash
 
-source terminal/env
-
-# nodejs environment
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-
-if command -v nvm > /dev/null; then
-    nvm install 12
-else
-    echo "nvm not installed"
-
+if ! nvm --version; then
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-    nvm install 12
-    npm install -g yaml-language-server
-    npm install -g neovim
 fi
+
+nvm install 12
+npm install -g neovim
