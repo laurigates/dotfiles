c0f81f47401ace3ce4a659a329aabb94750b0989
Lauri Gates <lauri.gates@frozenbyte.com>
2020-06-15 13:19:36 +0300

    Refactor/fix installation
    The base.yaml was run for each separate config, so I split all the
    remaining stuff from it to sensible locations.

--- setup/cleanup.sh
+++ setup/cleanup.sh
@@ -4,19 +4,7 @@
 # are the default files and not symlinked to the
 # dotfiles repo files
 
-conffiles=(
-    "~/.config/dunst/dunstrc"
-    "~/.profile"
-    "~/.bashrc"
-    "~/.xinitrc"
-    "~/.Xresources"
-    "~/.config/compton.conf"
-    "~/.config/pet"
-    "~/.config/coc/extensions/package.json"
-    "~/.config/nvim/coc-settings.json"
-)
-
-for conffile in "${conffiles[@]}"
+for arg
 do
   if eval "test -f $conffile || test -d $conffile && test ! -L $conffile"; then
     eval "mv -v $conffile $conffile.old"
