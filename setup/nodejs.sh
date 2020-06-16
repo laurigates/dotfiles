da9328e9ebf14425b72ac73a7955679befad44ec
Lauri Gates <lauri.gates@gmail.com>
2020-06-16 16:10:13 +0300

    Fix node setup script

--- setup/nodejs.sh
+++ setup/nodejs.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+source terminal/env
+
 if ! nvm --version; then
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
