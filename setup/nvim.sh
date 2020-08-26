73e7c46a166eccc8aa81677c6919b12d486b084e
Lauri Gates <lauri.gates@gmail.com>
2020-08-26 21:02:24 +0300

    Fix nvim setup script
    Removed leftover stuff from earlier coc extension install method.

--- setup/nvim.sh
+++ setup/nvim.sh
@@ -2,11 +2,6 @@
 
 if command -v nvim > /dev/null; then
     nvim --headless +PlugInstall +PlugUpdate +UpdateRemotePlugins +qall
-    # TODO: Fix this command so that it doesn't leave a bunch of dependencies lying
-    # around under ~/.config/coc/extensions. The CocInstall command only leaves the
-    # plugins there, but I have not succeeded in running CocInstall headless.
-    # nvim +CocInstall +qa --headless
-    cd ~/.config/coc/extensions && npm install --ignore-scripts --no-package-lock --only=production
 else
     echo "nvim not installed"
 fi
