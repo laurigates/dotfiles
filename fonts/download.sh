4fcab3a9be874406034e88ac18a61192343b09ae
Lauri Gates <lauri.gates@gmail.com>
2020-09-15 17:55:39 +0300

    Remove font scripts
    The nerd-fonts-hack or equivalent distro package is a cleaner way to
    install the fonts and cuts the time it takes to install dotfiles.

--- fonts/download.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-declare -a styles=('Bold' 'Bold Italic' 'Italic' 'Regular')
-
-cd ~/dotfiles/fonts/hack
-for style in "${styles[@]}"
-do
-     wget -N "https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Hack/${style// /}/complete/Hack%20$style%20Nerd%20Font%20Complete.ttf"
-done
--- setup/fonts.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if command -v fc-cache > /dev/null; then
-    ../fonts/download.sh
-    fc-cache -f -v
-else
-    echo "fc-cache not found"
-fi
