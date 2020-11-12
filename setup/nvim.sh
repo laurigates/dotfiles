07cfe934d9a5ae2afb4e8e80fa050f0b1a2b55df
Lauri Gates <lauri.gates@frozenbyte.com>
2020-11-12 12:04:16 +0200

    Fix nvim setup

--- setup/nvim.sh
+++ setup/nvim.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+
 if command -v nvim > /dev/null; then
-    nvim --headless --noplugin +PlugClean! +PlugInstall +PlugUpdate +qa
-    nvim --headless +UpdateRemotePlugins +qa
     if command -v npm > /dev/null; then
         npm install -g neovim
     fi
+    nvim --headless --noplugin +PlugClean! +PlugInstall +PlugUpdate +qa
+    nvim --headless +UpdateRemotePlugins +qa
 else
     echo "nvim not installed"
 fi
