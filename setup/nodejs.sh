20a3f5c9cb69d31e1145c16d057b6d8555adfa07
Lauri Gates <lauri.gates@gmail.com>
2020-11-12 16:37:26 +0200

    Fix nodejs setup and update nvm install script

--- setup/nodejs.sh
+++ setup/nodejs.sh
@@ -1,8 +1,10 @@
 #!/bin/zsh
 
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+
 if ! nvm --version; then
-    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
-    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.0/install.sh | bash
 fi
 
 nvm install 15
