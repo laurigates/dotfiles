406080e5b1e2ad2b6498bfff14194ad4713fd93f
Lauri Gates <lauri.gates@frozenbyte.com>
2020-11-18 14:49:08 +0200

    Fix rust setup
    Use a better check to see if rust is installed.
    
    Strip binaries even if rust was already installed.

--- setup/rust.sh
+++ setup/rust.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
-if ! cargo --version > /dev/null 2>&1; then
+export PATH="$HOME/.cargo/bin:$PATH"
+
+if ! rustup --version > /dev/null 2>&1; then
     wget --quiet -O rustup-init.sh https://sh.rustup.rs
     chmod +x rustup-init.sh
     ./rustup-init.sh --no-modify-path -y && rm rustup-init.sh
 
-    # only run strip if binaries exist
-    if [ $(find ~/.cargo/bin -type f | wc -l) -gt 0 ]; then
-        strip ~/.cargo/bin/*
-    else
-        echo "No cargo binaries found. Skipping stripping them."
-    fi
 else
-    echo "cargo not installed"
+    rustup update
+fi
+
+# only run strip if binaries exist
+if [ $(find ~/.cargo/bin -type f | wc -l) -gt 0 ]; then
+    strip ~/.cargo/bin/*
+else
+    echo "No cargo binaries found. Skipping stripping them."
 fi
