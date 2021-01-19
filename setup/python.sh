f4217b97fc56cbad122abcead844735de758f92b
Lauri Gates <lauri.gates@frozenbyte.com>
2021-01-19 09:43:44 +0200

    Fix pip upgrade
    Upgrade the user install of pip instead of globally.

--- setup/python.sh
+++ setup/python.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if command -v pip > /dev/null; then
-    pip install --upgrade pip
+    pip install --user --upgrade pip
     pip install --user --upgrade -q -r requirements.txt
 else
     echo "pip3 not installed"
