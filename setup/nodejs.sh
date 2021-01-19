32f3f6afbf2db7b273f4ef54f93f2af710aef203
Lauri Gates <lauri.gates@gmail.com>
2021-01-19 07:08:02 +0200

    Replace nvm with nodenv
    Nodenv loads a lot faster than nvm, affecting startup of the shell and vim sessions.
    
    Nodenv is based on rbenv, which I already use so it simplifies things.

--- setup/nodejs.sh
+++ setup/nodejs.sh
@@ -1,12 +1,11 @@
 #!/bin/zsh
 
-export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+export PATH="$HOME/.nodenv/shims:$HOME/.nodenv/bin:$PATH"
 
-if ! nvm --version > /dev/null 2>&1; then
-    curl -s -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.0/install.sh | bash
-    export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+if ! nodenv --version > /dev/null 2>&1; then
+    cd ~/.nodenv && src/configure && make -C src
 fi
 
-nvm install --no-progress 15
+nodenv install 15.6.0
+nodenv global 15.6.0
+npm i -g neovim
