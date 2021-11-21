d58bbc7bf93d67ea0ea9f7200e1f18932a278af5
Lauri Gates <lauri.gates@gmail.com>
2021-11-21 16:22:16 +0200

    Fix nodejs setup

--- setup/nodejs.sh
+++ setup/nodejs.sh
@@ -1,10 +1,12 @@
 #!/bin/zsh -e
 
+VERSION=15.6.0
+
 if ! nodenv --version > /dev/null 2>&1; then
     cd ~/.nodenv && src/configure && make -C src
 fi
 
-nodenv install 15.6.0
-nodenv global 15.6.0
+nodenv install --skip-existing $VERSION
+nodenv global $VERSION
 npm i -g neovim
 nodenv rehash
