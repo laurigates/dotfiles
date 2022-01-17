2c5f08cbbdadab56611db2febf748e437caf4f54
Lauri Gates <lauri.gates@forumvirium.fi>
2022-01-17 12:29:34 +0200

    Improve nvim install usability
    Moved the install_nvim definition to zshenv so nvim can be easily
    upgraded from the terminal.
    
    Piped the download straight to tar so the archive isn't left over after
    installation.

--- setup/nvim.sh
+++ setup/nvim.sh
@@ -8,26 +8,6 @@ if nvim --version > /dev/null 2>&1; then
     nvim --headless +UpdateRemotePlugins +qa
 else
     echo "Neovim is not installed!"
+    # Defined in ~/.zshenv
     install_nvim
 fi
-
-install_nvim () {
-    if read -q "choice?Press Y/y to install neovim binaries to your home directory: "; then
-        case "$(uname)" in
-          'Darwin') OS=macos ;;
-          'Linux') OS=linux64 ;;
-        esac
-
-        # Should add the option to choose whether to install globally
-        INSTALL_DIR=/usr/local
-        INSTALL_DIR=~/.local
-
-        if [[ -v OS ]]; then
-          wget https://github.com/neovim/neovim/releases/download/nightly/nvim-$OS.tar.gz
-          # If installing globally, sudo is needed
-          tar xaf nvim-$OS.tar.gz --strip-components=1 -C $INSTALL_DIR
-        fi
-    else
-       echo "Skipping neovim installation."
-    fi
-}
