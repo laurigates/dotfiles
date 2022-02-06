54fb7ceec699c4d1f0aabfcfac219c75bedf076f
Lauri Gates <lauri.gates@gmail.com>
2022-02-06 20:05:33 +0200

    Improve neovim setup script
    - Removing PlugUpdate seems to get rid of nvim-treesitter compilation
      errors and speeds things up. It's probably better to update plugins
      manually on existing installations so that the changelogs can also be
      seen.
    - Install neovim every time so the latest version is always used. This
      will probably be an issue in environments where neovim has been
      installed using a package manager and will require further changes.

--- setup/nvim.sh
+++ setup/nvim.sh
@@ -1,14 +1,12 @@
 #!/bin/zsh -e
 
-if nvim --version > /dev/null 2>&1; then
-    if npm --version > /dev/null 2>&1; then
-        npm install -g neovim
-    fi
-    nvim --headless --noplugin +PlugClean! +qa
-    nvim --headless +PlugInstall! +PlugUpdate! +qa
-    nvim --headless +UpdateRemotePlugins +qa
-else
-    echo "Neovim is not installed!"
-    # Defined in ~/.zshenv
-    install_nvim
+# Always run install_nvim so it's at the latest version.
+# Too many things break otherwise.
+# Defined in ~/.zshenv
+install_nvim
+if npm --version > /dev/null 2>&1; then
+npm install -g neovim
 fi
+nvim --headless --noplugin +PlugClean! +qa
+nvim --headless --noplugin +PlugInstall! +qa
+nvim --headless +UpdateRemotePlugins +qa
