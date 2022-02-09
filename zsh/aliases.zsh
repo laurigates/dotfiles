ba43289701ea9d8fbc8283295815eff2abad6116
Lauri Gates <lauri.gates@forumvirium.fi>
2022-02-09 09:59:55 +0200

    Update aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,16 +2,14 @@
 type nvim >/dev/null 2>&1 && alias vim="nvim"
 
 # make common r10k deploy easier
-alias deploy='sudo r10k deploy environment -v info'
+# alias deploy='sudo r10k deploy environment -v info'
 
 # easy vimrc editing
 alias vimrc='vim ~/dotfiles/vim/init.vim'
 
 # easy zshrc editing
 alias zshrc='vim ~/dotfiles/zsh/zshrc'
-
-# use bat if it exists
-#type bat >/dev/null 2>&1 && alias cat="bat"
+alias zshenv='vim ~/dotfiles/zsh/zshenv'
 
 # fzf preview
 alias preview="fzf --preview 'bat --color \"always\" {}'"
