4705b4d80f574c141ee1fa0d92a7531be91e5072
Lauri Gates <lauri.gates@forumvirium.fi>
2022-04-29 15:29:08 +0300

    Update vimrc alias
    Updated the alias to open the new lua config.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -5,7 +5,7 @@ type nvim >/dev/null 2>&1 && alias vim="nvim"
 # alias deploy='sudo r10k deploy environment -v info'
 
 # easy vimrc editing
-alias vimrc='vim ~/dotfiles/vim/init.vim'
+alias vimrc='vim ~/dotfiles/neovim/init.lua'
 
 # easy zshrc editing
 alias zshrc='vim ~/dotfiles/zsh/zshrc'
