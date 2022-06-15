72a7a68c84a2050b25021b8c3bb5caa0a07d81a8
Lauri Gates <lauri.gates@forumvirium.fi>
2022-06-15 15:59:45 +0300

    Remove oh-my-zsh
    Inspired mainly by the following article, I decided to try removing
    oh-my-zsh from my setup and did some cleanup in the process. This
    removes quite a bit of unnecessary bloat and mystery hidden behind
    oh-my-zsh magic.
    
    https://ianyepan.github.io/posts/moving-away-from-ohmyzsh/

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,17 +1,19 @@
 # use nvim if it exists
 type nvim >/dev/null 2>&1 && alias vim="nvim"
 
-# make common r10k deploy easier
-# alias deploy='sudo r10k deploy environment -v info'
-
 # easy vimrc editing
 alias vimrc='vim ~/dotfiles/neovim/init.lua'
 
 # easy zshrc editing
-alias zshrc='vim ~/dotfiles/zsh/zshrc'
+alias zshrc='vim ~/.zshrc && source ~/.zshrc'
 alias zshenv='vim ~/dotfiles/zsh/zshenv'
+
+# use bat if it exists
+#type bat >/dev/null 2>&1 && alias cat="bat"
 
 # fzf preview
 alias preview="fzf --preview 'bat --color \"always\" {}'"
 
 alias icat="kitty +kitten icat"
+
+alias ls='ls --color=auto'
