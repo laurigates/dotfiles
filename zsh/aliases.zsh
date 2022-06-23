193bd1abca3bec165ec09aad33b5ab059b173ec7
Lauri Gates <lauri.gates@forumvirium.fi>
2022-06-23 10:14:44 +0300

    Update zsh aliases
    Copied the git aliases that I have been using from the oh-my-zsh git
    plugin.
    
    Use lsd to replace ls and tree. Might revert the ls alias if it causes
    issues in practice.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -16,4 +16,32 @@ alias preview="fzf --preview 'bat --color \"always\" {}'"
 
 alias icat="kitty +kitten icat"
 
-alias ls='ls --color=auto'
+# alias ls='ls --color=auto'
+
+alias ls='lsd'
+alias tree='lsd --tree'
+
+### Git aliases
+# Mostly cherry-picked from the oh-my-zsh git plugin:
+# https://github.com/ohmyzsh/ohmyzsh/blob/master/plugins/git/git.plugin.zsh
+
+alias ga='git add'
+alias gc='git commit -v'
+alias gl='git pull'
+alias gp='git push'
+
+alias gb='git branch'
+alias gba='git branch -a'
+alias gbd='git branch -d'
+
+alias gd='git diff'
+alias gdca='git diff --cached'
+alias gdcw='git diff --cached --word-diff'
+
+alias gm='git merge'
+
+alias gst='git status'
+
+alias gsw='git switch'
+
+alias gupa='git pull --rebase --autostash'
