25607f9215c755f04dc50ad4ce44845c33dd5834
Lauri Gates <lauri.gates@forumvirium.fi>
2023-01-27 16:17:09 +0200

    Improve rust setup script
    - Use curl instead of wget for macos compatibility
    - Generate completions for rustup and cargo

--- setup/rust.sh
+++ setup/rust.sh
@@ -1,13 +1,17 @@
 #!/bin/zsh -e
 
 if ! rustup --version > /dev/null 2>&1; then
-    wget --quiet -O rustup-init.sh https://sh.rustup.rs
+    curl -so rustup-init.sh https://sh.rustup.rs
     chmod +x rustup-init.sh
     ./rustup-init.sh --no-modify-path -y && rm rustup-init.sh
-
 else
     rustup update
 fi
+
+# Update completions definition
+mkdir -p ~/.zfunc
+rustup completions zsh > ~/.zfunc/_rustup
+rustup completions zsh cargo > ~/.zfunc/_cargo
 
 # only run strip if binaries exist
 if [ $(find ~/.cargo/bin -type f | wc -l) -gt 0 ]; then
