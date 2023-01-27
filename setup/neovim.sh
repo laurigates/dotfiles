cf2d5af0c131c48976278f90625d39230fc108cb
Lauri Gates <lauri.gates@forumvirium.fi>
2023-01-27 16:18:36 +0200

    Improve neovim setup script
    - Snapshot is now created of neovim plugins before upgrading them
    - Tree-sitter parsers are installed and updated
    - LSP configs are installed

--- setup/neovim.sh
+++ setup/neovim.sh
@@ -8,4 +8,9 @@ if npm --version > /dev/null 2>&1; then
     npm install -g neovim
 fi
 
-nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
+# Installs and updates tree-sitter parsers.
+# Create a snapshot of the current plugins, then clean and update them.
+# The autocommand is used to quit neovim after PackerSync has finished.
+# The commands use the synchronous versions so that they work in headless mode.
+nvim --headless -c "LspInstall --sync tsserver terraformls yamlls ansiblels bashls dockerls sumneko_lua" -c q
+nvim --headless -c 'TSInstallSync all' -c 'TSUpdateSync' -c "PackerSnapshot $(date -I)" -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
