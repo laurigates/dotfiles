d6e71ed67068bed0e69c364c894cd4486aacea6e
Lauri Gates <lauri.gates@forumvirium.fi>
2024-05-06 15:37:00 +0300

    feat(neovim): add keybinding for oil floating window

--- neovim/lua/plugins/oil.lua
+++ neovim/lua/plugins/oil.lua
@@ -1,9 +1,16 @@
 -- Neovim file explorer: edit your filesystem like a buffer
 return {
-	{
-		"stevearc/oil.nvim",
-		opts = {},
-		-- Optional dependencies
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-	},
+  {
+    "stevearc/oil.nvim",
+    opts = {},
+    keys = {
+      {
+        "<leader>o",
+        "<cmd>lua require('oil').open_float()<cr>",
+        desc = "Oil open floating window",
+      },
+    },
+    -- Optional dependencies
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+  },
 }
