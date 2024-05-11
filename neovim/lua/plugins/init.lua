0043ac94f3da7df488de6894176dd607b894624f
Lauri Gates <lauri.gates@forumvirium.fi>
2024-05-11 07:06:37 +0300

    refactor: normalize init.lua indentation to spaces

--- neovim/lua/plugins/init.lua
+++ neovim/lua/plugins/init.lua
@@ -1,43 +1,43 @@
 return {
-	"folke/which-key.nvim",
-	{
-		"folke/neoconf.nvim",
-		cmd = "Neoconf",
-		opts = {},
-	},
-	{
-		-- Plugin completions don't seem to work
-		-- An addition that would be nice: Support goto-definition (gd/gD) on plugin names. This could jump to the plugin code.
-		-- If K is pressed, display popup with info about plugin e.g. description
-		"folke/neodev.nvim",
-		opts = {},
-	},
-	{
-		"folke/noice.nvim",
-		event = "VeryLazy",
-		opts = {
-			-- add any options here
-		},
-		dependencies = {
-			-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-			"MunifTanjim/nui.nvim",
-			-- OPTIONAL:
-			--   `nvim-notify` is only needed, if you want to use the notification view.
-			--   If not available, we use `mini` as the fallback
-			"rcarriga/nvim-notify",
-		},
-	},
-	{
-		"folke/tokyonight.nvim",
-		lazy = false,
-		priority = 1000,
-		opts = {},
-	},
-	{
-		"windwp/nvim-autopairs",
-		event = "InsertEnter",
-		opts = {}, -- this is equalent to setup({}) function
-	},
+  "folke/which-key.nvim",
+  {
+    "folke/neoconf.nvim",
+    cmd = "Neoconf",
+    opts = {},
+  },
+  {
+    -- Plugin completions don't seem to work
+    -- An addition that would be nice: Support goto-definition (gd/gD) on plugin names. This could jump to the plugin code.
+    -- If K is pressed, display popup with info about plugin e.g. description
+    "folke/neodev.nvim",
+    opts = {},
+  },
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {
+      -- add any options here
+    },
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      "MunifTanjim/nui.nvim",
+      -- OPTIONAL:
+      --   `nvim-notify` is only needed, if you want to use the notification view.
+      --   If not available, we use `mini` as the fallback
+      "rcarriga/nvim-notify",
+    },
+  },
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+  },
+  {
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    opts = {}, -- this is equalent to setup({}) function
+  },
   {
     'echasnovski/mini.nvim',
     version = false,
