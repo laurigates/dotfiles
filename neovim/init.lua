26f5e9117cd8b256feb32ff92ec129dfa7b42d87
Lauri Gates <lauri.gates@forumvirium.fi>
2024-05-11 07:09:55 +0300

    fix: adjust indentation in init.lua for consistency

--- neovim/init.lua
+++ neovim/init.lua
@@ -1,14 +1,14 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -16,7 +16,6 @@ vim.g.mapleader = " "
 
 -- Load plugins from directory
 require("lazy").setup("plugins")
-
 vim.cmd([[colorscheme tokyonight]])
 
 require("core/settings")
