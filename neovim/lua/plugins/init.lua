bf09750db6ce6cc5fa27c35ee62fd5f0aebc858c
Lauri Gates <lauri.gates@forumvirium.fi>
2024-05-11 07:01:44 +0300

    feat(neovim): add mini.nvim plugins to init config
    Replacing a few plugins with mini.nvim alternatives for their
    simplicity.

--- neovim/lua/plugins/init.lua
+++ neovim/lua/plugins/init.lua
@@ -48,4 +48,14 @@ return {
 		main = "ibl",
 		opts = {},
 	},
+  {
+    'echasnovski/mini.nvim',
+    version = false,
+    config = function()
+      require('mini.surround').setup()
+      -- require('mini.completion').setup()
+      require('mini.ai').setup()
+      require('mini.indentscope').setup()
+    end
+  },
 }
