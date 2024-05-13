5a6b04c99ec9119d5d4ed7a03efa0cd9d30f1e2c
Lauri Gates <lauri.gates@forumvirium.fi>
2024-05-13 09:26:44 +0300

    feat: add disable autoformat feature & codespell config
    The formatter was correcting shortcat to shortcut so I configured ways
    to deal with such issues. Ignored words can be configured in codespellrc
    and conform formatter can be disabled with `:FormatDisable` and enabled
    with `:FormatEnable`.

--- neovim/lua/plugins/conform.lua
+++ neovim/lua/plugins/conform.lua
@@ -13,11 +13,32 @@ return {
         -- Use a sub-list to run only the first available formatter
         -- javascript = { { "prettierd", "prettier" } },
       },
-      format_on_save = {
-        -- I recommend these options. See :help conform.format for details.
-        lsp_fallback = true,
-        timeout_ms = 500,
-      },
+      format_on_save = function(bufnr)
+        -- Disable with a global or buffer-local variable
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+          return
+        end
+        return { timeout_ms = 500, lsp_fallback = true }
+      end,
     },
+    config = function()
+      vim.api.nvim_create_user_command("FormatDisable", function(args)
+        if args.bang then
+          -- FormatDisable! will disable formatting just for this buffer
+          vim.b.disable_autoformat = true
+        else
+          vim.g.disable_autoformat = true
+        end
+      end, {
+        desc = "Disable autoformat-on-save",
+        bang = true,
+      })
+      vim.api.nvim_create_user_command("FormatEnable", function()
+        vim.b.disable_autoformat = false
+        vim.g.disable_autoformat = false
+      end, {
+        desc = "Re-enable autoformat-on-save",
+      })
+    end
   },
 }
