5b88b3a56875f9aa308c39b97c1d357b0a409887
Lauri Gates <lauri.gates@forumvirium.fi>
2024-05-28 21:57:36 +0300

    feat: add nvim-biscuits plugin to Neovim configuration

--- neovim/lua/plugins/init.lua
+++ neovim/lua/plugins/init.lua
@@ -52,4 +52,27 @@ return {
   "tpope/vim-speeddating",
   "tpope/vim-eunuch",
   "tpope/vim-rhubarb",
+  {
+    "code-biscuits/nvim-biscuits",
+    opts = {
+      cursor_line_only = true,
+      default_config = {
+        -- max_length = 12,
+        -- min_distance = 5,
+        prefix_string = "📎"
+      },
+      -- language_config = {
+      --   html = {
+      --     prefix_string = " 🌐 "
+      --   },
+      --   javascript = {
+      --     prefix_string = " ✨ ",
+      --     max_length = 80
+      --   },
+      --   python = {
+      --     disabled = true
+      --   }
+      -- }
+    }
+  },
 }
