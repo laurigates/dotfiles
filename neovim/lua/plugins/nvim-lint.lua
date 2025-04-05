0f384e98b90666debd97c85f6179c224984181e2
Lauri Gates <lauri.gates@forumvirium.fi>
2025-04-05 13:27:07 +0300

    fix(nvim-lint): Run linters on BufWritePost
    This commit configures nvim-lint to run linters automatically on BufWritePost.

--- neovim/lua/plugins/nvim-lint.lua
+++ neovim/lua/plugins/nvim-lint.lua
@@ -50,16 +50,16 @@ return {
 
     -- Complex configuration using config function
     config = function()
-        vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-          callback = function()
-            -- try_lint without arguments runs the linters defined in `linters_by_ft`
-            -- for the current filetype
-            require("lint").try_lint()
+      vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+        callback = function()
+          -- try_lint without arguments runs the linters defined in `linters_by_ft`
+          -- for the current filetype
+          require("lint").try_lint()
 
-            -- You can call `try_lint` with a linter name or a list of names to always
-            -- run specific linters, independent of the `linters_by_ft` configuration
-            -- require("lint").try_lint("cspell")
-          end,
+          -- You can call `try_lint` with a linter name or a list of names to always
+          -- run specific linters, independent of the `linters_by_ft` configuration
+          -- require("lint").try_lint("cspell")
+        end,
       })
     end,
 
@@ -73,5 +73,5 @@ return {
     -- end,
 
     -- Uncomment section(s) above to enable them
-}
+  },
 }
