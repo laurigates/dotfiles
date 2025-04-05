2b2af2c1da9c0cdb31f75514c56488c9ff5bea9a
Lauri Gates <lauri.gates@forumvirium.fi>
2025-04-05 13:24:50 +0300

    feat(nvim): add vectorcode plugin

---
+++ neovim/lua/plugins/vectorcode.lua
@@ -0,0 +1,6 @@
+return {
+  "Davidyz/VectorCode",
+  version = "*", -- optional, depending on whether you're on nightly or release
+  dependencies = { "nvim-lua/plenary.nvim" },
+  cmd = "VectorCode", -- if you're lazy-loading VectorCode
+}
