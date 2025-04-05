eddb9c6a22256c98131a65c8bf2b24ec92f1f33d
Lauri Gates <lauri.gates@forumvirium.fi>
2025-04-05 13:28:10 +0300

    feat(git): remove nvim-web-devicons dependency
    The plugin doesn't directly depend on nvim-web-devicons; telescope does.

--- neovim/lua/plugins/git.lua
+++ neovim/lua/plugins/git.lua
@@ -7,7 +7,6 @@ return {
     dependencies = {
       "nvim-lua/plenary.nvim",
       "nvim-telescope/telescope.nvim",
-      "nvim-tree/nvim-web-devicons",
     },
     opts = {
       default_merge_method = "squash",
