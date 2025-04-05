d9fbdc6bfb58f2b9a9f8c1137e8c71569708838e
Lauri Gates <lauri.gates@forumvirium.fi>
2025-04-05 13:30:13 +0300

    fix(conform): Change format keybind to <leader>fo
    The original keybind, <leader>f, was shadowing another keybind.

--- neovim/lua/plugins/conform.lua
+++ neovim/lua/plugins/conform.lua
@@ -5,7 +5,7 @@ return {
   keys = {
     {
       -- Customize or remove this keymap to your liking
-      "<leader>f",
+      "<leader>fo",
       function()
         require("conform").format({ async = true })
       end,
