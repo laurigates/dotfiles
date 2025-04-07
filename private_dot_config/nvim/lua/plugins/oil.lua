c7ced54cba746d27c5de5d802271fa2ce2c5fbe8
Lauri Gates <lauri.gates@forumvirium.fi>
2025-04-07 20:23:50 +0300

    fix(oil.lua): fix indentation in oil.lua plugin configuration
    The indentation was inconsistent, this commit fixes that.

--- private_dot_config/nvim/lua/plugins/oil.lua
+++ private_dot_config/nvim/lua/plugins/oil.lua
@@ -1,14 +1,14 @@
 -- Neovim file explorer: edit your filesystem like a buffer
 return {
-  {
-    "stevearc/oil.nvim",
-    opts = {},
-    keys = {
-      {
-        "<leader>o",
-        "<cmd>lua require('oil').open_float()<cr>",
-        desc = "Oil open floating window",
-      },
-    },
-  },
+	{
+		"stevearc/oil.nvim",
+		opts = {},
+		keys = {
+			{
+				"<leader>o",
+				"<cmd>lua require('oil').open_float()<cr>",
+				desc = "Oil open floating window",
+			},
+		},
+	},
 }
