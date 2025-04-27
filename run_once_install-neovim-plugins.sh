ca55d60922f01166406c1712f6e59801844675aa
Lauri Gates <lauri.gates@forumvirium.fi>
2025-04-27 20:40:49 +0300

    cleanup: Remove unused files
    This commit removes the following unused files:
    
    - `private_dot_config/private_kitty/kitty.conf`
    - `run_once_install-neovim-plugins.sh`
    - `run_once_macos.zsh`

--- run_once_install-neovim-plugins.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Install, cleanup and update neovim plugins
-nvim --headless "+Lazy! sync" "+MasonUpdate" +qa
-
-# The checkhealth just runs forever
-# Check neovim health
-# nvim --headless "+checkhealth" +qa
-
