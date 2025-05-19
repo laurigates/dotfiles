544a8725297a285fae94be3a7c20351ce8865ece
Lauri Gates <lauri.gates@forumvirium.fi>
2025-05-19 09:50:06 +0300

    feat: Add autotag plugin and configure terraformls

--- private_dot_config/nvim/lua/core/lsp.lua
+++ private_dot_config/nvim/lua/core/lsp.lua
@@ -1,28 +1,3 @@
--- local navic = require("nvim-navic")
---
--- local on_attach = function(client, bufnr)
---   if client.server_capabilities.documentSymbolProvider then
---     navic.attach(client, bufnr)
---   end
--- end
---
--- local capabilities = require("blink.cmp").get_lsp_capabilities()
---
--- require("mason").setup()
--- require("mason-lspconfig").setup()
---
--- require("mason-lspconfig").setup_handlers({
---   -- The first entry (without a key) will be the default handler
---   -- and will be called for each installed server that doesn't have
---   -- a dedicated handler.
---   function(server_name) -- default handler (optional)
---     require("lspconfig")[server_name].setup({
---       on_attach = on_attach,
---       capabilities = capabilities,
---     })
---   end,
--- })
-
 -- Add the same capabilities to ALL server configurations.
 -- Refer to :h vim.lsp.config() for more information.
 vim.lsp.config("*", {
@@ -32,6 +7,14 @@ vim.lsp.config("*", {
 require("mason").setup()
 require("mason-lspconfig").setup({
   -- ensure_installed = { "rust_analyzer", "lua_ls" }
+})
+
+vim.lsp.config("terraformls", {
+  settings = {
+    ["terraform"] = {
+      experimentalFeatures = { prefillRequiredFields = true, validateOnSave = true },
+    },
+  },
 })
 
 vim.keymap.set({ "n", "x" }, "<leader>a", '<cmd>lua require("fastaction").code_action()<CR>', { buffer = bufnr })
--- private_dot_config/nvim/lua/plugins/treesitter.lua
+++ private_dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,120 +1,119 @@
 -- Nvim Treesitter configurations and abstraction layer
 return {
-	{
-		"RRethy/nvim-treesitter-textsubjects",
-		dependencies = { "nvim-treesitter/nvim-treesitter" },
-	},
-	{
-		"nvim-treesitter/nvim-treesitter-textobjects",
-		dependencies = { "nvim-treesitter/nvim-treesitter" },
-	},
-	{
-		"nvim-treesitter/nvim-treesitter",
-		main = "nvim-treesitter.configs",
-		build = ":TSUpdate",
-		opts = {
-			modules = {},
-			auto_install = true,
-			ignore_install = {},
-			ensure_installed = {
-				"regex",
-				"bash",
-				"c",
-				"diff",
-				"dockerfile",
-				"git_config",
-				"git_rebase",
-				"gitattributes",
-				"gitcommit",
-				"gitignore",
-				"graphql",
-				"http",
-				"json",
-				"lua",
-				"markdown",
-				"markdown_inline",
-				"python",
-				"query",
-				"terraform",
-				"toml",
-				"vim",
-				"vimdoc",
-				"xml",
-				"yaml",
-			},
-			sync_install = false,
-			highlight = {
-				enable = true,
-				disable = function(lang, bufnr)
-					-- Disable for large SQL files
-					return lang == "sql" and vim.api.nvim_buf_line_count(bufnr) > 50000
-				end,
-			},
-			indent = { enable = true },
-			incremental_selection = { enable = true },
-			textobjects = {
-				select = {
-					enable = true,
+  {
+    "windwp/nvim-ts-autotag",
+    event = "LazyFile",
+    opts = {},
+  },
+  {
+    "RRethy/nvim-treesitter-textsubjects",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    main = "nvim-treesitter.configs",
+    build = ":TSUpdate",
+    opts = {
+      modules = {},
+      auto_install = true,
+      ignore_install = {},
+      ensure_installed = {
+        "regex",
+        "bash",
+        "c",
+        "diff",
+        "dockerfile",
+        "git_config",
+        "git_rebase",
+        "gitattributes",
+        "gitcommit",
+        "gitignore",
+        "graphql",
+        "http",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "query",
+        "terraform",
+        "toml",
+        "vim",
+        "vimdoc",
+        "xml",
+        "yaml",
+      },
+      sync_install = false,
+      highlight = { enable = true },
+      indent = { enable = true },
+      incremental_selection = { enable = true },
+      textobjects = {
+        select = {
+          enable = true,
 
-					-- Automatically jump forward to textobj, similar to targets.vim
-					lookahead = true,
+          -- Automatically jump forward to textobj, similar to targets.vim
+          lookahead = true,
 
-					keymaps = {
-						-- You can use the capture groups defined in textobjects.scm
-						["af"] = "@function.outer",
-						["if"] = "@function.inner",
-						["ac"] = "@class.outer",
-						-- You can optionally set descriptions to the mappings (used in the desc parameter of
-						-- nvim_buf_set_keymap) which plugins like which-key display
-						["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
-						-- You can also use captures from other query groups like `locals.scm`
-						["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
-					},
-					-- You can choose the select mode (default is charwise 'v')
-					--
-					-- Can also be a function which gets passed a table with the keys
-					-- * query_string: eg '@function.inner'
-					-- * method: eg 'v' or 'o'
-					-- and should return the mode ('v', 'V', or '<c-v>') or a table
-					-- mapping query_strings to modes.
-					selection_modes = {
-						["@parameter.outer"] = "v", -- charwise
-						["@function.outer"] = "V", -- linewise
-						["@class.outer"] = "<c-v>", -- blockwise
-					},
-					-- If you set this to `true` (default is `false`) then any textobject is
-					-- extended to include preceding or succeeding whitespace. Succeeding
-					-- whitespace has priority in order to act similarly to eg the built-in
-					-- `ap`.
-					--
-					-- Can also be a function which gets passed a table with the keys
-					-- * query_string: eg '@function.inner'
-					-- * selection_mode: eg 'v'
-					-- and should return true of false
-					include_surrounding_whitespace = true,
-				},
-				lsp_interop = {
-					enable = true,
-					border = "none",
-					floating_preview_opts = {},
-					peek_definition_code = {
-						["<leader>df"] = "@function.outer",
-						["<leader>dF"] = "@class.outer",
-					},
-				},
-			},
-			textsubjects = {
-				enable = true,
-				prev_selection = ",", -- (Optional) keymap to select the previous selection
-				keymaps = {
-					["."] = "textsubjects-smart",
-					[";"] = "textsubjects-container-outer",
-					["i;"] = {
-						"textsubjects-container-inner",
-						desc = "Select inside containers (classes, functions, etc.)",
-					},
-				},
-			},
-		},
-	},
+          keymaps = {
+            -- You can use the capture groups defined in textobjects.scm
+            ["af"] = "@function.outer",
+            ["if"] = "@function.inner",
+            ["ac"] = "@class.outer",
+            -- You can optionally set descriptions to the mappings (used in the desc parameter of
+            -- nvim_buf_set_keymap) which plugins like which-key display
+            ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+            -- You can also use captures from other query groups like `locals.scm`
+            ["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
+          },
+          -- You can choose the select mode (default is charwise 'v')
+          --
+          -- Can also be a function which gets passed a table with the keys
+          -- * query_string: eg '@function.inner'
+          -- * method: eg 'v' or 'o'
+          -- and should return the mode ('v', 'V', or '<c-v>') or a table
+          -- mapping query_strings to modes.
+          selection_modes = {
+            ["@parameter.outer"] = "v", -- charwise
+            ["@function.outer"] = "V", -- linewise
+            ["@class.outer"] = "<c-v>", -- blockwise
+          },
+          -- If you set this to `true` (default is `false`) then any textobject is
+          -- extended to include preceding or succeeding whitespace. Succeeding
+          -- whitespace has priority in order to act similarly to eg the built-in
+          -- `ap`.
+          --
+          -- Can also be a function which gets passed a table with the keys
+          -- * query_string: eg '@function.inner'
+          -- * selection_mode: eg 'v'
+          -- and should return true of false
+          include_surrounding_whitespace = true,
+        },
+        lsp_interop = {
+          enable = true,
+          border = "none",
+          floating_preview_opts = {},
+          peek_definition_code = {
+            ["<leader>df"] = "@function.outer",
+            ["<leader>dF"] = "@class.outer",
+          },
+        },
+      },
+      textsubjects = {
+        enable = true,
+        prev_selection = ",", -- (Optional) keymap to select the previous selection
+        keymaps = {
+          ["."] = "textsubjects-smart",
+          [";"] = "textsubjects-container-outer",
+          ["i;"] = {
+            "textsubjects-container-inner",
+            desc = "Select inside containers (classes, functions, etc.)",
+          },
+        },
+      },
+    },
+  },
 }
